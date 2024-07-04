fe3e3359076e2ed281a5a82d68e546c3f29bc98d
1140444693@qq.com <1140444693@qq.com>
2024-07-04 17:47:12 +0800

    完成系统管理-字典数据api

--- src/app/api/admin/system/dict/data/route.ts
+++ src/app/api/admin/system/dict/data/route.ts
@@ -1,23 +1,40 @@
 import { trim } from "lodash";
 import { NextRequest } from "next/server";
 import api from "@/lib/response";
-import {  getSystemDictTypeByType } from "@/model/systemDictType";
+import { getSystemDictTypeByType } from "@/model/systemDictType";
 import { now } from "@/lib/date";
 import { createSystemDictData, getSystemDictDataList } from "@/model/systemDictData";
+import { Prisma } from "@prisma/client";
 
 // 获取字典列表
 export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl;
-  const type = searchParams.get("type") || '';
-  if(!trim(type)) {
+  const type = searchParams.get("type") || "";
+  if (!trim(type)) {
     return api.error("字典类型不能为空！");
   }
-  
+
+  const where: Prisma.sysDictDataWhereInput = {
+    type
+  };
+
+  const label = searchParams.get("label") || "";
+  if (trim(label)) {
+    where.label = label;
+  }
+  const value = searchParams.get("value") || "";
+  if (trim(value)) {
+    where.value = value;
+  }
+
+  const status = searchParams.get("status") || "";
+  if (trim(status)) {
+    where.status = Number(status);
+  }
+
   const list = await getSystemDictDataList({
-    where: {
-      type
-    }
-  })
+    where
+  });
   return api.success("获取成功！", list);
 }
 
--- src/model/systemDictData.ts
+++ src/model/systemDictData.ts
@@ -1,8 +1,19 @@
+import { formatFields } from "@/lib/date";
 import prisma from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 
-// 获取字典列表
-export function getSystemDictDataList({
+// 获取字典数据总数
+export async function getSystemDictDataCount(type: string) {
+  const count = await prisma.sysDictData.count({
+    where: {
+      type
+    }
+  });
+  return count;
+}
+
+// 获取字典数据列表
+export async function getSystemDictDataList({
   where,
   orderBy
 }: {
@@ -12,21 +23,58 @@ export function getSystemDictDataList({
     | Prisma.sysDictDataOrderByWithRelationInput[]
     | undefined;
 }) {
-  const systemDict = prisma.sysDictData.findMany({
+  const systemDict = await prisma.sysDictData.findMany({
     where,
     orderBy
   });
+  formatFields(systemDict, [
+    { fromField: "createdAt", toField: "createTime", format: "YYYY-MM-DD HH:mm:ss" }
+  ]);
   return systemDict;
 }
 
 // 创建系统字典数据
-export function createSystemDictData({
+export async function createSystemDictData({
   data
 }: {
   data: Prisma.XOR<Prisma.sysDictDataCreateInput, Prisma.sysDictDataUncheckedCreateInput>;
 }) {
-  const systemDict = prisma.sysDictData.create({
+  const systemDict = await prisma.sysDictData.create({
     data
+  });
+  return systemDict;
+}
+
+// 根据字典数据id获取数据
+export async function getSystemDictDataById(id: number) {
+  const systemDict = await prisma.sysDictData.findUnique({
+    where: {
+      id
+    }
+  });
+  return systemDict;
+}
+
+// 更新系统字典数据
+export async function updateSystemDictData(
+  id: number,
+  data:  Prisma.XOR<Prisma.sysDictDataUpdateInput, Prisma.sysDictDataUncheckedUpdateInput>
+) {
+  const systemDict = await prisma.sysDictData.update({
+    where: {
+      id
+    },
+    data
+  });
+  return systemDict;
+}
+
+// 根据字典数据id删除删除
+export async function deleteSystemDictDataById(id: number) {
+  const systemDict = await prisma.sysDictData.delete({
+    where: {
+      id
+    }
   });
   return systemDict;
 }
