6543f169ec43be2a344b198e8929f55799459909
Return_归来 <1140444693@qq.com>
2025-04-14 16:00:52 +0800

    增加serverless部署

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,21 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: "standalone",
   experimental: {
     optimizePackageImports: [
-      '@ant-design/pro-components',
-      '@lexical/react',
-       '@lexical/utils',
-       '@lexical/selection',
-       '@radix-ui/react-dialog',
-       '@radix-ui/react-icons',
-       '@heroicons/react',
-       '@radix-ui/react-slot',
-       'lexical',
-       'lodash'
-      ],
-  },
+      "@ant-design/pro-components",
+      "@lexical/react",
+      "@lexical/utils",
+      "@lexical/selection",
+      "@radix-ui/react-dialog",
+      "@radix-ui/react-icons",
+      "@heroicons/react",
+      "@radix-ui/react-slot",
+      "lexical",
+      "lodash"
+    ]
+  }
 };
 
 export default nextConfig;
